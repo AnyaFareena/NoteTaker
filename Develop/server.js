626b3d940a6e7450eccd773d63363cd4a4715694
Anya <anyaanto1909@gmail.com>
2019-12-06 10:56:57 -0500

    adding server file

---
+++ Develop/server.js
@@ -0,0 +1,55 @@
+var fs = require("fs");
+var express = require("express");
+var path = require("path");
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+var notes = [];
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(express.static("db"));
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("/index", function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.post("/api/notes", function (req, res) {
+    fs.readFile(path.join(__dirname, "/db/db.json"), function (err, data) {
+        notes = JSON.parse(data);
+        req.body.id = notes.length + 1;
+        notes.push(req.body)
+        console.log(notes);
+        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(notes), function () {
+        });
+    });
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+    fs.readFile(path.join(__dirname, "/db/db.json"), function (err, data) {
+        notes = JSON.parse(data);
+        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(notes.filter(note => note.id != req.params.id)), () => { console.log(`Note deleted`) });
+
+    });
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
